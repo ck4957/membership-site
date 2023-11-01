083b7eb7560e9176bd3f3a99701c768902af5420
Chirag Kular <kular_chirag@yahoo.com>
2023-11-01 19:23:06 +0000

    Add Amplify NextJS App

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hero } from "../ui-components";
+import { View } from "@aws-amplify/ui-react";
+
+function index() {
+  return (
+    <View>
+      <Hero />
+    </View>
+  );
+}
+export default index;
